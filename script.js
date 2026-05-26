76df920318bf67d510bdb3160252fa2b4d0a25c7
Dharamveer Singh Rana <132247521+dhaamii@users.noreply.github.com>
2026-05-26 12:54:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const restart = document.querySelector(".restart");
+const game = document.querySelector(".game");
+const number = document.querySelector(".number");
+const check = document.querySelector(".check");
+const show = document.querySelector(".show");
+const result = document.querySelector(".result");
+const counter = document.querySelector(".counter");
+const guess = document.querySelector(".guess");
+
+const random = Math.trunc(Math.random() * 20) + 1;
+console.log(random);
+
+const finalResult = function () {
+  {
+    result.textContent = `Hurray 🥳, You Won, Correct Guess.`;
+    document.body.style.backgroundColor = "green";
+    show.textContent = random;
+  }
+};
+
+function disableCheckButton() {
+  check.disabled = true;
+}
+
+check.addEventListener("click", function () {
+  if (
+    (number.textContent = parseInt(number.value)) &&
+    number.textContent > 0 &&
+    number.textContent < 21
+  ) {
+    counter.textContent++;
+    if (counter.textContent < 5) {
+      guess.textContent = `Guess ${5 - counter.textContent} more times`;
+      if (number.textContent > random) {
+        result.textContent = `Try low number.`;
+      } else if (number.textContent < random) {
+        result.textContent = `Try high number.`;
+      } else {
+        finalResult();
+        disableCheckButton();
+      }
+    } else if (counter.textContent === "5") {
+      guess.textContent = `Guess Over`;
+      disableCheckButton();
+
+      if (number.textContent != random) {
+        counter.textContent = "00";
+        guess.textContent = "STATUS";
+        result.textContent = `Sorry😪, You Lost, Try Again`;
+        document.body.style.backgroundColor = "red";
+      } else {
+        finalResult();
+      }
+    }
+  }
+  document.querySelector(".restart").addEventListener("click", function () {
+    window.location.reload();
+  });
+});
